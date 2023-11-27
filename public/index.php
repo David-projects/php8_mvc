fd3d975d30017df8147e4a539b74a4d4af67264d
David Theobald <davidtheo@gmail.com>
2023-11-27 08:12:00 +0000

    Setup the MVC project and start project.
    Even so the is a MVC framework I will be making a full Project from this framework.

---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?
+
+#loading boostrip to start MVC framework
+$app = include __DIR__ ."/../src/App/bootstrip.php";
+
+$app->run();
---
+++ src/App/bootstrip.php
@@ -0,0 +1,22 @@
+<?
+/**
+* load and configurer  the files needed to run the application
+*/
+declare(strict_types=1);
+
+/**
+ * Autoloader setup, update this file up updating composer.json
+ * then run composer dump-autoload. this will create a new 
+ * vendor/composer/autoload_psr4.php file
+ * 
+ * PSR are the standards used by this project please read for the standards
+ * https://www.php-fig.org/
+ * 
+ */
+require __DIR__ . "/../../vendor/autoload.php";
+
+use Framework\App;
+
+$app = new App();
+
+return $app;
